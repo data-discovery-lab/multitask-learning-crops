073c346408e4cb850f590178c7164949de8b99f1
Hoang Long <litpuvn@gmail.com>
2018-09-27 00:24:59 -0500

    working mtl keras

--- mtl_keras.py
+++ mtl_keras.py
@@ -1,28 +1,60 @@
+from keras.callbacks import Callback
 from keras.engine.input_layer import Input
 from keras.engine.training import Model
 from keras.layers.core import Dense
-
+# from sklearn.metrics import
 import numpy as np
 import matplotlib.pyplot as plt
+import pandas as pd
 
-n_row = 1000
-x1 = np.random.randn(n_row)
-x2 = np.random.randn(n_row)
-x3 = np.random.randn(n_row)
-dat = np.array([x1, x2, x3]).transpose()
+
+class TestCallback(Callback):
+    def __init__(self, test_data):
+        self.test_data = test_data
+
+    def on_epoch_end(self, epoch, logs={}):
+        x, y = self.test_data
+        loss, acc = self.model.evaluate(x, y, verbose=0)
+        print('\nTesting loss: {}, acc: {}\n'.format(loss, acc))
+
+
+df = pd.read_csv('data/data_excel_converted.csv', delimiter=',')
+
+samples = 475
+features = 42
+tasks = 3
+test_size = int(0.2*samples)
+
+### shape = (samples, features)
+dat = []
+for i in range(50):
+    if i < 8:
+        continue
+    dat.append(df[df.columns[i]])
+
+dat = np.array(dat).transpose()
 
 # Generate indexes of test and train
-idx_list = np.linspace(0, 999, num=1000)
-idx_test = np.random.choice(n_row, size=200, replace=False)
+idx_list = np.linspace(0, samples-1, num=samples)
+idx_test = np.random.choice(samples, size=test_size, replace=False)
 idx_train = np.delete(idx_list, idx_test).astype('int')
 
 # Split data into test and train
 dat_train = dat[idx_train, :]
+labels = df[df.columns[3:6]]
+
+label_train_1 = labels.values[idx_train, 0] # year 2000
+label_train_2 = labels.values[idx_train, 1] # year 2001
+label_train_3 = labels.values[idx_train, 2] # year 2002
+
 dat_test = dat[idx_test, :]
+label_test_1 = labels.values[idx_test, 0] # year 2000
+label_test_2 = labels.values[idx_test, 1] # year 2000
+label_test_3 = labels.values[idx_test, 2] # year 2000
 
+x = Input(shape=(features, ))
+shared = Dense(units=4*features)(x)
 
-x = Input(shape=(3, ))
-shared = Dense(units=32)(x)
 sub1 = Dense(units=16, activation='relu')(shared)
 sub2 = Dense(units=16, activation='relu')(shared)
 sub3 = Dense(units=16, activation='relu')(shared)
@@ -34,22 +66,18 @@ out3 = Dense(units=1, activation='linear')(sub3)
 
 model = Model(inputs=x, outputs=[out1, out2, out3])
 
-num_labels = dat_train.shape[0]
-label1 = np.random.rand(num_labels, 1)
-label2 = np.random.rand(num_labels, 1)
-label3 = np.random.rand(num_labels, 1)
-
-label_test = np.random.rand(200, 1)
 
 # Compiling the model using 'adam' optimizer and MSE as loss function
-model.compile(optimizer='adam', loss='mean_squared_error')
+model.compile(optimizer='adam', loss='mean_squared_error',  metrics=['mse', 'mae', 'mape'])
 
-model.fit(x=dat_train, y=[label1, label2, label3], epochs=5, batch_size=20)
+#muti_outputs shape= tasks x train_samples
+callbacks = []
+model.fit(x=dat_train, y=[label_train_1, label_train_2, label_train_3], epochs=100, batch_size=20)
 
-pred1, pred2, pred3 = model.predict(dat_test)
-
-plot_x = dat_test[:, 0]
-plot_y = np.array(pred1) - label_test
-plt.scatter(x=dat_test[:, 0], y=np.array(pred1) - label_test)
-
-plt.show()
+# pred1, pred2, pred3 = model.predict(dat_test)
+#
+# plot_x = dat_test[:, 0]
+# plot_y = np.array(pred1) - label_test_1
+# plt.scatter(x=dat_test[:, 0], y=np.array(pred1) - label_test_1)
+#
+# plt.show()

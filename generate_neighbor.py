2820c83f2d75aabbd2a6083a13dc846424d0e645
Hoang Long <litpuvn@gmail.com>
2018-10-06 16:08:12 -0500

    add code to generate neighbors

---
+++ generate_neighbor.py
@@ -0,0 +1,155 @@
+import numpy as np
+import csv
+
+class Cell:
+    def __init__(self, r_id, c_id, label, active=False):
+        self.r_id = r_id
+        self.c_id = c_id
+        self.label = label
+        self.active = active
+
+    def get_row_id(self):
+        return self.r_id
+
+    def get_col_id(self):
+        return self.c_id
+
+    def get_index(self):
+        return self.r_id * 25 + self.c_id
+
+    def is_active(self):
+        return self.active
+
+    def get_label(self):
+        return self.label
+
+class Row:
+    def __init__(self, id, start_label, start_valid_label, end_valid_label):
+        self.id = id
+        self.start_valid_label = start_valid_label
+        self.start_label = start_label
+        self.end_valid_label = end_valid_label
+        self.cells = np.array([])
+        self.row_size = 25
+
+    def get_id(self):
+        return self.id
+
+    def add_cell(self, cell):
+        self.cells = np.append(self.cells, cell)
+
+    def get_cells(self):
+        return self.cells
+
+    def generate_cells(self):
+        for col in range(25):
+            label = self.start_label + col
+            active = False
+            if self.id != 9 and self.id != 11 and self.id != 12 and self.id != 13:
+                active = True
+                if label < self.start_valid_label:
+                    active = False
+            elif self.id == 9:
+                active = True
+                if col >= 1 and col <= 3:
+                    active = False
+            elif self.id == 11:
+                active = True
+                if col >= 12 and col <= 13:
+                    active = False
+            elif self.id == 12:
+                active = True
+                if col >= 11 and col <= 13:
+                    active = False
+                if col >= 14:
+                    label = label - 1
+            elif self.id == 13:
+                active = True
+                if col >= 12 and col <= 13:
+                    active = False
+
+            cell = Cell(self.id, col, label, active)
+            self.add_cell(cell)
+
+
+row_configs = [
+    Row(0, 3009, 3018, 3024),
+    Row(1, 3035, 3042, 3053),
+    Row(2, 3067, 3072, 3086),
+    Row(3, 3103, 3108, 3123),
+    Row(4, 3142, 3147, 3163),
+    Row(5, 3182, 3187, 3163),
+    Row(6, 3225, 3230, 3248),
+    Row(7, 3270, 3275, 3293),
+    Row(8, 3317, 3321, 3341),
+    Row(9, 3367, 3367, 3391),
+    Row(10, 3421, 3421, 3445),
+    Row(11, 3475, 3475, 3499),
+    Row(12, 3528, 3528, 3551),
+    Row(13, 3580, 3580, 3604),
+    Row(14, 3634, 3634, 3658),
+    Row(15, 3687, 3687, 3711),
+    Row(16, 3738, 3739, 3762),
+    Row(17, 3788, 3789, 3811),
+    Row(18, 3837, 3839, 3860),
+    Row(19, 3883, 3885, 3905),
+    Row(20, 3928, 3931, 3950),
+    Row(21, 3969, 3973, 3990),
+    Row(22, 4006, 4011, 4026),
+    Row(23, 4039, 4045, 4057),
+    Row(24, 4067, 4075, 4083),
+]
+
+cell_map = dict()
+## Generate data
+for index, r in enumerate(row_configs):
+    r.generate_cells()
+    cells = r.get_cells()
+    for c in cells:
+        c_index = c.get_index()
+        if c_index not in cell_map:
+            cell_map[c_index] = c
+
+## compute neighbors size=theta
+for neighbor_size in range(1, 6):
+
+    with open('data/nb_size_' + str(neighbor_size), 'w') as csv_file:
+        writer = csv.writer(csv_file, delimiter=',')
+        writer.writerow(["source", "neighbor", "distance"])
+
+        for r in row_configs:
+            cells = r.get_cells()
+            for c in cells:
+                if not c.is_active():
+                    continue
+
+                r_id = c.get_row_id()
+                c_id = c.get_col_id()
+
+                nb_r_min = r_id - neighbor_size
+                nb_c_min = c_id - neighbor_size
+                nb_r_max = r_id + neighbor_size
+                nb_c_max = c_id + neighbor_size
+                for nb_r in range(nb_r_min, nb_r_max+1):
+                    if nb_r < 0:
+                        continue
+                    for nb_c in range(nb_c_min, nb_c_max+1):
+                        if nb_c < 0:
+                            continue
+
+                        if r_id == nb_r and c_id == nb_c:
+                            print("same cell")
+                            continue
+
+                        nb_index = nb_r * 25 + nb_c
+                        if nb_index not in cell_map:
+                            print("row:", nb_r, "col:", nb_c, "is not an active cell")
+                            continue
+
+                        nb_cell = cell_map[nb_index]
+                        current_distance = max([abs(nb_r - r_id), abs(nb_c - c_id)])
+                        writer.writerow([c.get_label(), nb_cell.get_label(), current_distance])
+
+
+
+        ## write to file
